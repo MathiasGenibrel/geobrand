616ab13e0b4f1df69cf915fae93434630bb5a8eb
MathiasGenibrel <mathias.geni@gmail.com>
2022-06-29 18:00:25 +0200

    feat(styled-components)
    - Add theme config
    - Add theme provider to index.js
    - Add globalStyles for reset styles

---
+++ src/config/themes.js
@@ -0,0 +1,31 @@
+/**
+ * This object allows the configuration of all the basic styles of the app
+ */
+const themes = {
+  colors: {
+    primary: "#006d77",
+    secondary: "#83c5be",
+    tertiary: "#edf6f9",
+    fourth: "#0b132b",
+  },
+  spacing: {
+    small: "0.5rem",
+    medium: "1rem",
+    large: "2rem",
+  },
+  font: {
+    size: {
+      small: "12px",
+      medium: "16px",
+      large: "24px",
+    },
+    weight: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+    },
+  },
+};
+
+export default themes;
