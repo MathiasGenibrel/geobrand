ec8871940056d18a9535d7a3616b9c77f5e23065
MathiasGenibrel <mathias.geni@gmail.com>
2022-06-30 00:48:08 +0200

    feat(wip)
    - Use useFetch Hook

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import { useFetch } from "../hooks/useFetch";
 
 import { useInput } from "../hooks/useInput";
 
@@ -10,13 +11,31 @@ export const Landing = () => {
   const [activity, activityHandler] = useInput("");
   const [radius, radiusHandler] = useInput(0);
 
+  const [cityResponse, citySubmitHandler] = useFetch(
+    "GET",
+    "https://geo.api.gouv.fr/communes",
+    200
+  );
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const cityRequestContent = {
+      nom: city,
+      codePostal: 66300,
+      fields: "nom,code,codesPostaux,centre,departement,region,population",
+      format: "json",
+      geometry: "centre",
+    };
+
+    citySubmitHandler({ params: cityRequestContent });
+  };
+
   const formContent = {
     button: {
       submit: "Afficher les entreprises",
     },
-    submitHandler: (event) => {
-      event.preventDefault();
-    },
+    submitHandler: submitHandler,
     inputs: [
       {
         type: "text",
@@ -51,6 +70,7 @@ export const Landing = () => {
     <>
       <Header />
       <Hero formContent={formContent} />
+      {JSON.stringify(cityResponse)}
     </>
   );
 };
